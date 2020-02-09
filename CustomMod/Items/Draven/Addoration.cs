bae5103059dfb2e0af9235ccd6f04cc53ac1c0c8
TnS101 <54761133+TnS101@users.noreply.github.com>
2020-02-09 06:38:00 +0200

    Included Additional Items (Draven)

---
+++ CustomMod/Items/Draven/Addoration.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace CustomMod.Items.Draven
+{
+    public class Addoration : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Addoration");
+            Tooltip.SetDefault("Welcome to the league of Draaaaven!");
+        }
+
+        public override void SetDefaults()
+        {
+            item.consumable = true;
+            item.maxStack = 999;
+            item.useStyle = 4;
+            item.useTime = 1;
+            item.autoReuse = false;
+        }
+
+        public override void OnConsumeItem(Player player)
+        {
+            player.DoCoins(2);
+        }
+    }
+}
