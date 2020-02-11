042c51ebf103be7ec7f621ce7aeb8297a8dcfe9a
TnS101 <54761133+TnS101@users.noreply.github.com>
2020-02-11 08:52:12 +0200

    Added Projectiles

---
+++ CustomMod/Projectiles/Draven/SpinningAxeP.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CustomMod.Projectiles.Draven
+{
+    public class SpinningAxeP : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            projectile.CloneDefaults(ProjectileID.Bananarang);
+            aiType = 3;
+            projectile.Name = "Spinning Axe";
+            projectile.damage = 60;
+            projectile.width = 20;
+            projectile.height = 20;
+            projectile.penetrate = 1;
+            projectile.friendly = true;
+            projectile.tileCollide = true;
+            projectile.aiStyle = 1;
+            projectile.thrown = true;
+            projectile.extraUpdates = 1;
+            projectile.timeLeft = 600;
+        }
+
+        public override void Kill(int timeLeft)
+        {
+            if (Main.myPlayer == projectile.owner)
+            {
+                var item = Item.NewItem((int)projectile.position.X, (int)projectile.position.Y, projectile.width, projectile.height, mod.ItemType("SpinningAxe"), 1, false, 0, false, false);
+            }
+        }
+
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+        {
+            var random = new Random();
+            int num = random.Next(0, 2);
+            target.DeathSound = mod.GetLegacySoundSlot(SoundType.Custom, $"Sounds/Draven/Item/DravenPassive{num + 1}");
+
+            if (target.life < 1)
+            {
+                for (int i = 0; i <= 1; i++)
+                {
+                    var coin = Item.NewItem((int)projectile.position.X, (int)projectile.position.Y, projectile.width, projectile.height, ItemID.SilverCoin, 1, false, 0, false, false);
+                }
+            }
+        }
+
+        public override void AI()
+        {
+            projectile.ai[0] += 1f;
+            if (projectile.ai[0] >= 50f)       //how much time the projectile can travel before landing
+            {
+                projectile.velocity.Y = projectile.velocity.Y + 0.15f;    // projectile fall velocity
+                projectile.velocity.X = projectile.velocity.X * 0.99f;    // projectile velocity
+            }
+        }
+    }
+}
---
+++ CustomMod/Projectiles/Draven/StandAsideP.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CustomMod.Projectiles.Draven
+{
+    public class StandAsideP : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            projectile.CloneDefaults(ProjectileID.WoodenBoomerang);
+            aiType = ProjectileID.WoodenBoomerang;
+            projectile.Name = "Spinning Axe";
+            projectile.width = 20;
+            projectile.height = 20;
+            projectile.penetrate = 1;
+            projectile.friendly = true;
+            projectile.tileCollide = true;
+            projectile.aiStyle = 1;
+            projectile.thrown = true;
+            projectile.extraUpdates = 1;
+            projectile.noDropItem = false;
+            projectile.rotation = projectile.velocity.ToRotation() + MathHelper.PiOver2; // projectile sprite faces up
+            projectile.rotation = projectile.velocity.ToRotation(); // projectile faces sprite right
+            projectile.rotation += 0.4f * projectile.direction;
+            projectile.spriteDirection = projectile.direction;
+        }
+
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+        {
+            knockback = 5f;
+            target.stepSpeed -= 3;
+            ModPlayer player = mod.GetPlayer("MainPlayer");
+            player.player.AddBuff(mod.BuffType("StandAsideCD"),300);
+        }
+
+        public override void AI()
+        {
+            projectile.ai[0] += 1f;
+            if (projectile.ai[0] >= 50f)       //how much time the projectile can travel before landing
+            {
+                projectile.velocity.Y = projectile.velocity.Y + 0.15f;    // projectile fall velocity
+                projectile.velocity.X = projectile.velocity.X * 0.99f;    // projectile velocity
+            }
+        }
+    }
+}
