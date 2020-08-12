af9e1cbf7b37b5a0039181b7b3a8a93daf590844
TnS101 <54761133+TnS101@users.noreply.github.com>
2020-08-12 16:30:18 +0300

    Reorder Items

---
+++ CustomMod/Items/Consumeables/Buffs/HealthPotionBuff.cs
@@ -0,0 +1,19 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace CustomMod.Npcs.Store.Buffs
+{
+    public class HealthPotionBuff : ModBuff
+    {
+        public override void SetDefaults()
+        {
+            DisplayName.SetDefault("Healing from Potion");
+            Description.SetDefault("Regenerating Life!");
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+            player.lifeRegen += 2;
+        }
+    }
+}
